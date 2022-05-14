d1b8e0e9938946167980cd5dfeef7058cc839e47
LadyNunez <laadynz@hotmail.com>
2022-05-14 12:31:02 -0700

    IndexOf te da el indice #3 de la cadena

--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -16,14 +16,29 @@
 //Console.WriteLine(numeroConCeros);
 
 
-string nombre = "Lady";
-string apellido = "Nuñez";
-int edad = 33;
+//string nombre = "Lady";
+//string apellido = "Nuñez";
+//int edad = 33;
 
 //string nombreYEdad = nombre + "|" + apellido + "|" + edad;
 //Console.WriteLine(nombreYEdad);
 
 
-string nombreYEdad = $"{nombre} | {apellido} | {edad}" ;
-Console.WriteLine(nombreYEdad);
+//string nombreYEdad = $"{nombre} | {apellido} | {edad}" ;
+//Console.WriteLine(nombreYEdad);
 
+//string numeroString = "5 Lady";
+//int numeroInt = Convert.ToInt32 (numeroString);
+
+
+
+//int numeroInt = int.Parse(numeroString); 
+//Console.WriteLine(numeroInt);
+
+//string numeroString = "5";
+//int numeroInt = -1; 
+//bool pudo = int.TryParse(numeroString, out numeroInt);
+//Console.WriteLine(pudo);
+
+string cadena = "Holamundo";
+Console.WriteLine(cadena.IndexOf("a"));
