41c427f5fec4b78c954ff26d9c9c9ab1a05cad16
LadyNunez <laadynz@hotmail.com>
2022-05-14 11:39:40 -0700

    Para que sirve PadLeft

--- Ejemplo/Program.cs
+++ Ejemplo/Program.cs
@@ -1,5 +1,17 @@
-﻿string nombre = " Lady ";
-Console.WriteLine(nombre.Trim());
+﻿//string nombre = " ";
+//Console.WriteLine(nombre.Length);
 
+//if (string.IsNullOrWhiteSpace (nombre))
+//{
+//    Console.WriteLine("string vacio");
+//}
+//else
+//{
+//    Console.WriteLine(nombre);
+//}
 
+int numero = 9;
+string numeroConCeros = "99";
+numeroConCeros = numeroConCeros.PadLeft( 3, '$');
+Console.WriteLine(numeroConCeros);
 
